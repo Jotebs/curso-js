28925eb80e1767c24296f9d8029c112b6c217b93
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-10 15:07:30 -0300

    refazendo o exercicio do contador

--- exercicios/praticas/p3/script.js
+++ exercicios/praticas/p3/script.js
@@ -1,4 +1,4 @@
-function contar() {
+function contar(){
     let ini = document.getElementById('txtini')
     let fim = document.getElementById('txtfim')
     let passo = document.getElementById('txtpasso')
@@ -6,25 +6,25 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
     } else {
-        res.innerHTML = "Contando: "
+        res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
         if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
             p = 1
-            window.alert('Passo inválido! Considerando PASSO 1')
         }
-        if(i < f){
-            for(let c = i; c <= f; c += p){
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c<=f; c += p){
                 res.innerHTML += ` ${c} \u{1F449}`
             }
-            res.innerHTML += `Fim!`
-        }else{
-            for(let c=i; c>=f; c-=p){
+        } else {
+            // Contagem regressiva
+            for (let c = i; c>=f; c -= p){
                 res.innerHTML += ` ${c} \u{1F449}`
             }
-            res.innerHTML += `Fim!`
         }
-            
-    }   
+        res.innerHTML += `Fim! \u{1F3C1}`
+    }
 }
