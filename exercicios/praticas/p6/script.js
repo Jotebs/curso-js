5d18b297a8d9fbd5c8eda8281314868a804ac0cd
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-13 03:33:12 -0300

    refazendo para fixar na cabeça

--- exercicios/praticas/p6/script.js
+++ exercicios/praticas/p6/script.js
@@ -1,17 +1,17 @@
 let num = document.getElementById('txtn')
 let lista = document.getElementById('sellis')
-let item = document.createElement('option')
-let valores = [] //vetor para guardar os valores adicionados
+let res = document.getElementById('res')
+let valores = []
 
-function isNumero(n) { //verifica se o numero esta entre 1 e 100
-    if (Number(n) >= 1 && Number(n) <= 100) {
+function isNumero(n) { //verifica se o número está entre 1 e 100
+    if(Number(n) >= 1 && Number(n) <=100){
         return true
     } else {
         return false
     }
 }
 
-function inLista(n, l){  //verifica se o numero ja esta na lista
+function inLista(n, l){ //verifica se o número já está na lista
     if (l.indexOf(Number(n)) != -1){
         return true
     } else {
@@ -19,45 +19,39 @@ function inLista(n, l){  //verifica se o numero ja esta na lista
     }
 }
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) { //se for um numero valido e nao estiver na lista
-        valores.push(Number(num.value)) //adiciona o numero na lista
-        let item = document.createElement('option') //cria um elemento option
-        item.text = `Valor ${num.value} adicionado.` //cria o item da lista
-        lista.appendChild(item) //adiciona o item na lista
-        res.innerHTML = '' //limpa a res toda vez que um novo numero for adicionado
-        /*
-        Basicamente, a cada numero valido digitado, um novo elemento vai ser criado
-        e adicionado na lista de selecao (select)
-        */
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value)) //add o numero na lista
+        let item = document.createElement('option') //cria o elemento option
+        item.text = `valor ${num.value} adicionado.` //texto do option
+        lista.appendChild(item) //add o option na lista
     }else{
-        alert('Valor inválido ou ja cadastrado na lista')
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
-    num.value = '' 
-    num.focus() //o de cima limpa o campo, e esse aqui volta o foco para o campo
+    num.value = ''
+    num.focus()
+    res.innerHTML = ''
 }
 
-function finalizar() {
+function finalizar(){
     if (valores.length == 0){
-        alert('Adicione valores antes de finalizar!')
-    }else{
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
         let tot = valores.length
         valores.sort()
-        let maior = valores[tot - 1]
+        let maior = valores[valores.length - 1]
         let menor = valores[0]
         let soma = 0
-        let media = 0
-
-        for(let pos in valores){ //verifica cada valor dentro do vetor
+        for(let pos in valores){
             soma += valores[pos]
         }
-        media = soma / tot
+        let media = soma / tot
 
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
-        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-        res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`
-        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+        res.innerHTML += `<p>O maior número é ${maior}</p>`
+        res.innerHTML += `<p>O menor número é ${menor}</p>`
+        res.innerHTML += `<p>A soma dos números é ${soma}</p>`
+        res.innerHTML += `<p>A média dos números é ${media}</p>`
     }
 }
