71cd561db848d5b8a47521fe2c192b653d87be0a
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-13 03:37:56 -0300

    ultimo update

--- exercicios/praticas/p6/script.js
+++ exercicios/praticas/p6/script.js
@@ -1,7 +1,7 @@
-let num = document.getElementById('txtn')
-let lista = document.getElementById('sellis')
-let res = document.getElementById('res')
-let valores = []
+let num = document.getElementById('txtn') //variavel que recebe o input do número
+let lista = document.getElementById('sellis') //variavel da select onde os núm vai aparecer
+let res = document.getElementById('res') //variavel da div onde o resultado vai aparecer
+let valores = [] //array que vai armazenar os números adicionados
 
 function isNumero(n) { //verifica se o número está entre 1 e 100
     if(Number(n) >= 1 && Number(n) <=100){
@@ -20,7 +20,7 @@ function inLista(n, l){ //verifica se o número já está na lista
 }
 
 function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
+    if(isNumero(num.value) && !inLista(num.value, valores)){ //se o núm for valido e n tiver na lista
         valores.push(Number(num.value)) //add o numero na lista
         let item = document.createElement('option') //cria o elemento option
         item.text = `valor ${num.value} adicionado.` //texto do option
@@ -28,9 +28,9 @@ function adicionar(){
     }else{
         window.alert('Valor inválido ou já encontrado na lista.')
     }
-    num.value = ''
-    num.focus()
-    res.innerHTML = ''
+    num.value = '' //limpa o campo a cada numero adicionado
+    num.focus() //volta o foco para o campo de número
+    res.innerHTML = '' //limpa o resultado a cada novo número adicionado
 }
 
 function finalizar(){
@@ -38,16 +38,16 @@ function finalizar(){
         window.alert('Adicione valores antes de finalizar!')
     } else {
         let tot = valores.length
-        valores.sort()
+        valores.sort() //ordena os valores em ordem crescente
         let maior = valores[valores.length - 1]
         let menor = valores[0]
         let soma = 0
-        for(let pos in valores){
-            soma += valores[pos]
+        for(let pos in valores){ //percorre cada valor de cada posição
+            soma += valores[pos] //soma cada valor de cada posição ao 'soma', que é inicialmente 0
         }
         let media = soma / tot
 
-        res.innerHTML = ''
+        res.innerHTML = '' //cada vez que finalizar for clicado, o resultado anterior será apagado
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior número é ${maior}</p>`
         res.innerHTML += `<p>O menor número é ${menor}</p>`
