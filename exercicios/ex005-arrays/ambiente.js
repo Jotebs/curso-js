a0c457208494d0999c9610b6e66c89b8aba2326d
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-13 01:29:47 -0300

    aprendendo sobre arrays e um pouco de prática

---
+++ exercicios/ex005-arrays/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 8, 4, 5]
+num[3] = 6
+console.log(num)
+console.log(`o vetor tem ${num.length} posicoes`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+console.log(`agora com o vetor ordenado, temos ${num}`)
+
+let pos = num.indexOf(4)
+console.log(`O valor 4 está na posição ${pos}`)
+
---
+++ exercicios/praticas/p5/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5, 8, 6, 2, 9]
+console.log(` ${num}`)
+console.log(`foram digitados ${num.length} números`)
+num.sort()
+console.log(`o MAIOR número é ${num[num.length - 1]} e o MENOR é ${num[0]}`)
