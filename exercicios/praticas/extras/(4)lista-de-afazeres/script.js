7361776fe2230335cdc181cef40b44d204e7b101
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-14 03:20:00 -0300

    fix rápido de adição de itens repetidos

--- exercicios/praticas/extras/(4)lista-de-afazeres/script.js
+++ exercicios/praticas/extras/(4)lista-de-afazeres/script.js
@@ -1,14 +1,30 @@
 let task = document.getElementById('tarefa')
 let res = document.getElementById('res')
 let todo = document.getElementById('todo')
+let text = document.getElementById('text')
 
 function adicionar() {
     if (task.value.length == 0) { //validação para campo vazio
         res.innerHTML = 'Por favor, insira uma tarefa!'
     }else{
-        let item = document.createElement('option') //cria um elemento option
-        item.text = task.value
-        todo.appendChild(item)
+
+        let existe = false //variável de controle para verificar se a tarefa já existe
+
+        for(let i = 0; i < todo.length; i++) { //percorre o select
+            if (todo.options[i].text == task.value) { //se o texto de alguma option for igual ao valor inserido
+                existe = true                         //existe passa a ser true e quebra o loop
+                break
+            }
+        }
+
+        if (existe) {   //se a tarefa já existe, mostra essa mensagem
+            text.innerHTML = 'Tarefa já existe na lista!'
+        }else{
+            let item = document.createElement('option') //se não existir, prossegue com a criação do item
+            item.text = task.value
+            todo.appendChild(item)            
+        }
+
     }
 }
 
