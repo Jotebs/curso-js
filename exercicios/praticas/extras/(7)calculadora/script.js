d6706d8f9bc5e8202d680c52dca2b615387013ae
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-15 03:36:18 -0300

    update(acho que) final da calculadora, agora com casas decimais

--- exercicios/praticas/extras/(7)calculadora/script.js
+++ exercicios/praticas/extras/(7)calculadora/script.js
@@ -1,29 +1,37 @@
-let display = document.querySelector('.display') //variavel que recebe o display
+let display = document.querySelector('.display')
 
-let numeroAtual = ""; //variavel que armazena o numero
-let numeroAnterior = null; //variavel que armazena o numero anterior
-let operadorAtual = null; //variavel que armazena o operador
+let numeroAtual = "";
+let numeroAnterior = null; //ambos aqui recebem null pois no inicio n tem numero anterior, nem operador nem ponto
+let operadorAtual = null;
+let ponto = null;
 
 function addNumber(n){
-    numeroAtual += n; //adiciona o numero ao numero atual
+    numeroAtual += n; //concatena o n ao numeroAtual
     display.innerText = numeroAtual;
 }
 
-function setOperador(op){
-    if(numeroAtual == "") return; //verifica se o numero atual está vazio
-    if(operadorAtual !== null) return; //verifica se o operador atual é nulo
-    numeroAnterior = Number(numeroAtual); //o numero anterior recebe o valor do numero atual
-    operadorAtual = op;
-    numeroAtual = ""; //limpa o numero atual
-    display.innerText += operadorAtual; //mostra o operador no display
+function decimal(){
+    if (numeroAtual == "" || ponto !== null) return; //se o numero atual for vazio ou se o ponto for diferente de nulo, nao faz nada
+    ponto = "notNull"; //o ponto recebe notNull pra não ser adicionado mais de uma vez
+    numeroAtual += ".";
+    display.innerText += ".";
 }
 
-function calcular(){
-    if(numeroAtual === "" || operadorAtual === null) return; //verifica se o numero atual ou o operador atual é nulo
-    let atual = Number(numeroAtual); //o atual recebe o valor do numero atual
-    let resultado; //variavel que armazena o resultado
+function setOperador(op){
+    if (numeroAtual == "" || operadorAtual !== null) return; //se o numero atual estiver vazio ou se o operador atual n for nulo
+    numeroAnterior = Number(numeroAtual);
+    ponto = null; //o ponto recebe nulo pro segundo valor poder colocar o "."
+    operadorAtual = op; //o operador atual recebe o op
+    numeroAtual = ""; //o numero atual recebe vazio
+    display.innerText += operadorAtual;
+}
 
-    switch(operadorAtual){ //criei um switch case para cada operador
+function calcular() {
+    if(numeroAtual === "" || operadorAtual === null) return; //se o numero atual for vazio ou se o operador atual for nulo, nao faz nada
+    let atual = Number(numeroAtual); //o atual recebe o numero atual
+    let resultado;
+
+    switch(operadorAtual){ //dependendo do operador atual, a variavel resultado recebe o valor da conta abaixo
         case "+":
             resultado = numeroAnterior + atual;
             break;
@@ -37,15 +45,17 @@ function calcular(){
             resultado = numeroAnterior / atual;
             break;
     }
-    display.innerText = resultado; //mostra o resultado no display
-    numeroAnterior = null; //limpa o numero anterior
-    operadorAtual = null; //limpa o operador atual
-    numeroAtual = String(resultado); //o numero atual recebe o valor do resultado
+    display.innerText = resultado; //o display recebe o resultado
+    numeroAtual = String(resultado); //o numero atual recebe o resultado pro usuario poder mexer no resultado ainda
+    numeroAnterior = null;  //esses 3 volta as variáveis pro valor inicial
+    operadorAtual = null;
+    ponto = null;
 }
 
-function limpar(){ //limpa o display e todos os valores inseridos
-    numeroAtual = "";
+function limpar(){
+    numeroAtual = "";  //aqui volta todas as variáveis e valores iniciais
     numeroAnterior = null;
     operadorAtual = null;
     display.innerText = "";
+    ponto = null;
 }
