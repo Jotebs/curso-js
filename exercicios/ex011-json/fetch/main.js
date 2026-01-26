8b41e11595a8e0850959562f01aae7c40d7ef2bc
João Vitor Garcia <joaovitorpereiragarcia@gmail.com>
2026-01-26 00:51:30 -0300

    projeto de fetch com uma API de pokémon, com tratamento de erros com try...catch

--- exercicios/ex011-json/fetch/main.js
+++ exercicios/ex011-json/fetch/main.js
@@ -1,7 +1,24 @@
-async function buscarPokemon(){
-    const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu"); //pega os dados da API
-    const dados = await resposta.json();  //converte os dados para JSON e dá um parse automático
-    console.log(dados.name);
-    console.log(dados.types[0].type.name);
-}
 buscarPokemon();
+
+async function buscarPokemon(){ 
+
+    try{ // bloco try...catch para tratar erros
+        const pokemonNome = document.getElementById("pokemonNome").value.toLowerCase(); // obtém o nome do Pokémon do input e converte para minúsculas
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNome}`); // faz a requisição à API do Pokémon
+
+        if(!resposta.ok){
+            throw new Error(`Erro na requisição`); // lança um erro se a resposta não for bem-sucedida
+        }
+
+        const dados = await resposta.json(); // converte a resposta para JSON
+        const pokemonImagem = dados.sprites.front_default; // obtém a URL da imagem do Pokémon
+        const img = document.getElementById("pokemonSprite"); // obtém o elemento de imagem no HTML
+        
+        img.src = pokemonImagem; // define a fonte da imagem para a URL obtida
+        img.style.display = "block"; // torna a imagem visível
+    }
+    catch(erro){
+        console.error(error); // loga o erro no console
+        alert("Pokémon não encontrado. Verifique o nome e tente novamente."); // alerta o usuário sobre o erro
+    }
+}
